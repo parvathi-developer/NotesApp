18970e06434eec748c806d15df630143ab3f5634
parvathi-appdevloper <parvathi.appdevloper@gmail.com>
2024-07-17 16:19:20 +0530

    Chnaged the title and text

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hi Parvathi',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Hi Queen you have pushed the button this many times',
             ),
             Text(
               '$_counter',
