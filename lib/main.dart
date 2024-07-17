4c450454de89cfaaafe21fbc71c780a0eb57222b
parvathi-appdevloper <parvathi.appdevloper@gmail.com>
2024-07-17 15:45:33 +0530

    Changed the title and text

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-git
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
